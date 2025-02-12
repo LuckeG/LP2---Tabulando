5c12074ac64af17788e1e6ba965ebb772209b868
Lucas Gabriel <106352894+LuckeG@users.noreply.github.com>
2025-02-12 00:23:03 -0300

    Delete Projeto_Tabulando-main directory

--- Projeto_Tabulando-main/src/main/java/com/admin/projeto_tabulando/model/dao/AuthDao.java
+++
@@ -1,7 +0,0 @@
-package com.admin.projeto_tabulando.model.dao;
-
-import com.admin.projeto_tabulando.model.entities.Jogador;
-
-public interface AuthDao {
-    Jogador autenticarJogador(String usuario, String senha);
-}
